4da1931c008320adc2125e1dfd4bdf6ca8c7d414
krish3742 <krish3742@gmail.com>
2025-03-26 19:51:18 +0530

    Enhanced AI by giving it system instruction

--- CodexAI/Backend/src/app.js
+++ CodexAI/Backend/src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const aiRoutes = require("./routes/ai.routes");
+
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hey, Good morning!");
