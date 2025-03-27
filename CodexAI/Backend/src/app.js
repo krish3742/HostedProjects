53cdc425b02489666f8117b565a7223bffbdb6e7
krish3742 <krish3742@gmail.com>
2025-03-27 16:29:06 +0530

    Added cors in our app

--- CodexAI/Backend/src/app.js
+++ CodexAI/Backend/src/app.js
@@ -1,7 +1,9 @@
+const cors = require("cors");
 const express = require("express");
 const aiRoutes = require("./routes/ai.routes");
 
 const app = express();
+app.use(cors({ origin: `${process.env.CORS_ORIGIN_URL}`, credentials: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
